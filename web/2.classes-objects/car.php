5f8e51da157100630608e5804ac48fc83dbc62d4
Peter <myrvig@gmail.com>
2017-12-19 08:12:31 +0100

    op

--- web/2.classes-objects/car.php
+++ web/2.classes-objects/car.php
@@ -10,12 +10,14 @@ class Car {
 
     private $model;
     private $color;
-    public static $power;
+    public $power = 100;
 
     public function __construct($model, $color)
     {
         $this->model = $model;
         $this->color = $color;
+        $this->power = 100;
+        //self::$power = 200;
     }
 
     public function showCars()
@@ -23,19 +25,19 @@ class Car {
         echo "<dl>";
         echo "<dt>Model:</dt><dd>$this->model</dd >";
         echo "<dt>Color:</dt><dd>$this->color</dd >";
-        echo "<dt>Horse Power:</dt><dd>". self::$power. "</dd >";
+        echo "<dt>Power:</dt><dd>$this->power</dd >";
+        //echo "<dt>Horse Power:</dt><dd>". self::$power. "</dd >";
     }
 
-    public static function getPowers()
-    {
-        return self::$power;
-    }
 }
 
-Car::$power = 100;
+//Car::$power = 200;
+
 $aCar = new Car( "Audi", "Blue");
 $aCar->showCars();
 
+//echo self::$power;
+//echo Car::$power;
 
 
 
