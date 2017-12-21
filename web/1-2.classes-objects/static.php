138401f5d94dddb01e2f929ca990084148bae74a
Peter <myrvig@gmail.com>
2017-12-21 05:35:37 +0100

    op

--- web/1-2.classes-objects/static.php
+++ web/1-2.classes-objects/static.php
@@ -6,35 +6,11 @@
  * Time: 17:32
  */
 
-class Foo
+class test
 {
-    public static $my_static = 'foo';
-
-    public function staticValue() {
-        return self::$my_static;
-    }
+    public static $a;//Static variable
 }
-
-class Bar extends Foo
-{
-    public function fooStatic() {
-        return parent::$my_static;
-    }
-}
-
-
-print Foo::$my_static . "<br>";
-
-$foo = new Foo();
-print $foo->staticValue() . "<br>";
-//print $foo->my_static . "<br>";      // Undefined "Property" my_static
-
-print $foo::$my_static . "<br>";
-$classname = 'Foo';
-print $classname::$my_static . "<br>"; // As of PHP 5.3.0
-
-print Bar::$my_static . "<br>";
-$bar = new Bar();
-print $bar->fooStatic() . "<br>";
+test::$a = 5;
+echo test::$a;
 
 ?>
