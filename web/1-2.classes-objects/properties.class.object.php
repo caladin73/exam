185474524c061240c84a39b71a0393a813546f5b
Peter <myrvig@gmail.com>
2017-12-21 08:39:40 +0100

    op

--- web/1-2.classes-objects/properties.class.object.php
+++ web/1-2.classes-objects/properties.class.object.php
@@ -11,16 +11,17 @@ class exampleClass
     public static $foo;
     public $bar;
 
-    public function regularFunction()
-    {
-        echo $this->bar;
-    }
-
     public static function staticFunction()
     {
         echo self::$foo;
     }
 
+    public function regularFunction()
+    {
+        echo $this->bar;
+    }
+
+/*
     public static function anotherStatFn()
     {
         self::staticFunction();
@@ -30,16 +31,15 @@ class exampleClass
     {
         echo self::$foo;
     }
-
+*/
     // NOTE: As of PHP 5.3 using $this::$bar instead of self::$bar is allowed
 }
 
+// static method
 exampleClass::$foo = "Hello";
-
-$obj = new exampleClass();
-
-$obj->bar = "World!";
-
 exampleClass::staticFunction(); /* prints Hello */
 
+// regular method as $obj->bar
+$obj = new exampleClass();
+$obj->bar = "World!";
 $obj->regularFunction(); /* prints World! */
