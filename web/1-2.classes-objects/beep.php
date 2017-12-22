229d5edc5ee4d7948ec11c5e125a23b3d3b626aa
Peter <myrvig@gmail.com>
2017-12-22 08:15:31 +0100

    op

--- web/1-2.classes-objects/beep.php
+++ web/1-2.classes-objects/beep.php
@@ -13,12 +13,6 @@ class Car {
     public $company;
     public $color = 'beige';
     public $hasSunRoof = true;
-
-    // method that says hello
-    public function hello()
-    {
-        return "beep";
-    }
 }
 
 // Create an instance
@@ -34,16 +28,11 @@ $bmw -> color = 'Blue';
 $bmw -> hasSunRoof = false; //change the default value of sunroof from true to false
 
 //get the values
-echo "Company: " . $mercedes -> company. "<br>"; // BMW
-echo $mercedes -> color ."<br>"; // blue
-echo 'Has sunroof: '.(boolval($mercedes -> hasSunRoof) ? 'true' : 'false'). "<br><br>"; //uses an
+echo $mercedes -> company. "<br>"; // BMW
+echo $mercedes -> color ."<br>"; // Green
+echo 'Has sunroof: '.(boolval($mercedes -> hasSunRoof) ? 'yes' : 'no'). "<br><br>"; //checks if sunroof is set to true or false
 
-echo "Company: " . $bmw -> company. "<br>"; // BMW
-echo $bmw -> color ."<br>"; // blue
-echo 'Has sunroof: '.(boolval($bmw -> hasSunRoof) ? 'true' : 'false'). "<br><br>";
-
-// Use the methods to get a beep
-echo $bmw -> hello(). "<br>"; // beep
-echo $mercedes -> hello(); // beep
-
+echo $bmw -> company. "<br>"; // BMW
+echo $bmw -> color ."<br>"; // Blue
+echo 'Has sunroof: '.(boolval($bmw -> hasSunRoof) ? 'yes' : 'no'). "<br><br>"; //checks if sunroof is set to true or false
 ?>
