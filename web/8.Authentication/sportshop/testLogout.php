8088c03b5cad541b4f2ea920c0e9b863bfbf31ec
Peter <myrvig@gmail.com>
2017-12-31 16:26:23 +0100

    op

--- web/8.Authentication/sportshop/testLogout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    require_once './includes/Authentication.inc.php';
-    
-    if (Authentication::isAuthenticated()) {
-        Authentication::Logout();
-    }
-    header('Location: ./index.php?notAuth');
