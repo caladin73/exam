a51f68042e741891ac6b0c863aa1853730ad57f8
Peter <myrvig@gmail.com>
2018-01-03 22:20:53 +0100

    op

--- web/6.PHP.Database/PDO.php
+++ web/6.PHP.Database/PDO.php
@@ -9,7 +9,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-
+$dbname = "conn";
 
 /**
  * Notice that in PDO we have also specified a database (conn).
@@ -17,7 +17,7 @@ $password = "";
  * If no database is specified, an exception is thrown.
  */
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=conn", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
@@ -30,3 +30,5 @@ catch(PDOException $e)
 /**
  * A great benefit of PDO is that it has an exception class to handle any problems that may occur in our database queries. If an exception is thrown within the try{ } block, the script stops executing and flows directly to the first catch(){ } block
  */
+
+setcookie(cookie_name, cookie_value, [expiry_time], [cookie_path], [domain], [secure], [httponly]);
