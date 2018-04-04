09989bada3e4b607fcb47065ca65a1d4ab73a7d7
Peter <myrvig@gmail.com>
2018-04-04 21:01:36 +0200

    2 done

--- JS/2.Functions/scope.js
+++
@@ -1,9 +0,0 @@
-var funcA = function(){
-    var a = 1;
-    var funcB = function(){
-        var b = 2;
-        console.log(a, b); // outputs: 1, 2
-    }
-    console.log(a, b); // Error! b is not defined
-}
-funcA();
