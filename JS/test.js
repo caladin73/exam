7a0ad7d325246932fcc8d758f68f7f35fa8778e8
Peter <myrvig@gmail.com>
2018-04-05 15:57:53 +0200

    op

--- JS/test.js
+++ JS/test.js
@@ -1,5 +1,18 @@
-$("button").click(function(){
-    $("p").hide("slow", function(){
-        alert("The paragraph is now hidden");
-    });
-}); 
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="demo"></p>
+
+<script>
+var x = toCelsius(77);
+var text = "The temperature is " + x + " Celsius";
+
+function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+}
+
+</script>
+
+</body>
+</html>
