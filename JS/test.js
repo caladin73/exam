d4f9ba9ade4d8d8cf9ddcbbbaa314a72a3408459
Peter <myrvig@gmail.com>
2018-04-05 07:32:10 +0200

    op

--- JS/test.js
+++ JS/test.js
@@ -1,1 +1,24 @@
-var myElement = document.getElementById("intro");
+var id = setInterval(frame, 5);
+
+function frame() {
+    if (/* test for finished */) {
+        clearInterval(id);
+    } else {
+        /* code to change the element style */
+    }
+}
+
+
+document.getElementById(id).style.property = new style
+
+onclick=JavaScript
+onload
+onmouseover
+onchange
+onsubmit
+onkeypress
+document.addEventListener(event, function, useCapture)
+
+element.addEventListener("mouseover", myFunction);
+element.addEventListener("click", mySecondFunction);
+element.addEventListener("mouseout", myThirdFunction);
