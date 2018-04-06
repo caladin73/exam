bab4248d48663b9e01592e0f0e8c51392359b06f
Peter <myrvig@gmail.com>
2018-04-06 10:54:04 +0200

    op

--- JS/test.js
+++ JS/test.js
@@ -1,16 +1,5 @@
-var car = "Fiat";
-
-var car = {type:"Fiat", model:"500", color:"white"};
-
-var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
-var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
-
-function Person(first, last, age, eye) {
-    this.firstName = first;
-    this.lastName = last;
-    this.age = age;
-    this.eyeColor = eye;
-}
-
-var myFather = new Person("John", "Doe", 50, "blue");
-var myMother = new Person("Sally", "Rally", 48, "green");
+var c=document.getElementById("myCanvas");
+var ctx=c.getContext("2d");
+ctx.beginPath();
+ctx.arc(100,100,50,0,1.5*Math.PI);
+ctx.stroke();
