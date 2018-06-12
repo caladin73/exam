a439cc1ea1af360c6a21acf40259174ecec95f37
Peter Myrvig <myrvig@gmail.com>
2018-06-12 22:12:15 +0200

    op

--- Data_Integration/4.REST/DI.4.1/insert.php
+++ Data_Integration/4.REST/DI.4.1/insert.php
@@ -11,6 +11,6 @@ $dbh = DbH::getDbH();
 $sql = "INSERT INTO city (name, countrycode, district, population) VALUES ('$name', '$countrycode', '$district', '$population')";
 $res = $dbh->query($sql);
 
-var_dump($res);
+//var_dump($res);
 //SELECT * FROM city ORDER BY id DESC LIMIT 1;
 //DELETE FROM city ORDER BY id DESC LIMIT 1;
